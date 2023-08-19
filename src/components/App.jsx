c387426bf635bc5e2afe4a22a535bd957bde8d8c
10Artur <artyr11012002@gmail.com>
2023-08-19 19:45:36 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Button } from './Button/Button';
+import { GlobalStyle } from './GlobalStyle';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Searchbar } from './Searchbar/Searchbar';
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    query: '',
+    images: [],
+    page: 1,
+  };
+
+  changeQuery = newQuery => {
+    this.setState({
+      query: `${Date.now()}/${newQuery}`,
+      images: [],
+      page: 1,
+    });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.query !== this.state.query ||
+      prevState.page !== this.state.page
+    ) {
+      console.log(`${this.state.query}, ${this.state.page}`);
+    }
+  }
+
+  handleLoadMoreBtn = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <Searchbar />
+        <ImageGallery />
+        <Button />
+
+        <GlobalStyle />
+      </div>
+    );
+  }
+}
